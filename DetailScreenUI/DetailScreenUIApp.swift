d8a4667daf68da506a6a5eb9c0f18e337cbd9e49
Ghouse Azam <ghouseazam_s@nexteducation.in>
2026-02-17 16:54:56 +0530

    Initial Commit

---
+++ DetailScreenUI/DetailScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetailScreenUIApp.swift
+//  DetailScreenUI
+//
+//  Created by next on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DetailScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
